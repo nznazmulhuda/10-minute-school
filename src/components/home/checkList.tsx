c6e4e7c8ba998920fb4fbf8066e212f62d0eb851
Nazmul Huda <nznazmulhuda@users.noreply.github.com>
2025-07-25 21:35:02 +0600

    feat: add dynamic checklist section to homepage with backend-driven data

---
+++ src/components/home/checkList.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { useData } from "@/provider/data.provider";
+import { BanknoteArrowDown } from "lucide-react";
+import Image from "next/image";
+
+export default function CheckList() {
+  const { data } = useData();
+
+  return (
+    <Card className="overflow-hidden shadow-lg border border-slate-200 bg-white py-0">
+      <CardHeader className="bg-[#1d293d] py-2 pb-0">
+        <CardTitle className="flex items-center gap-3 text-white">
+          <div className="p-2 bg-white/20 rounded-lg backdrop-blur-sm">
+            <BanknoteArrowDown className="w-5 h-5" />
+          </div>
+          Check Lists
+        </CardTitle>
+      </CardHeader>
+
+      <CardContent className="p-4 pt-0">
+        <ul className="space-y-3">
+          {data?.checklist.map((item) => (
+            <li key={item.id} className="flex items-start gap-3">
+              {<Image src={item.icon} alt={item.text} width={18} height={18} />}
+              <span className={`text-sm text-gray-900`}>{item.text}</span>
+            </li>
+          ))}
+        </ul>
+      </CardContent>
+    </Card>
+  );
+}
