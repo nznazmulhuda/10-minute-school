e3e9ae9b39f6e1bf7e1c5e77cf9556b7ee89bbcd
Nazmul Huda <nznazmulhuda@users.noreply.github.com>
2025-07-25 16:50:56 +0600

    project layout updated, title, meta data and favicon updated✅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,43 @@
-export default function HomePage() {
+export default function Home() {
   return (
-    <main>
-      {/* banner */}
-      <section></section>
+    <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
+      <section className="grid grid-cols-6 gap-4">
+        {/* right side */}
+        <aside className="col-span-4 border-red-400 border h-screen">
+          {/* title */}
+          <section></section>
+
+          {/* description */}
+          <section></section>
+
+          {/* instructors */}
+          <section></section>
+
+          {/* how the course is laid out */}
+          <section></section>
+
+          {/* what you will learn by doing the course */}
+          <section></section>
+
+          {/* course exclusive feature */}
+          <section></section>
+
+          {/* course details */}
+          <section></section>
+        </aside>
+
+        {/* left side */}
+        <aside className="col-span-2 border-red-400 border h-screen">
+          {/* trailer */}
+          <section></section>
+
+          {/* cta */}
+          <section></section>
+
+          {/* check lists */}
+          <section></section>
+        </aside>
+      </section>
     </main>
   );
 }
